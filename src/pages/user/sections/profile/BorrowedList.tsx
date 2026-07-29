513fd746dd151227c54580cc04f10e8c341adc7d
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-07-29 10:41:12 +0700

    feat: implement user profile management; add profile retrieval and update functionality, enhance profile display with loading states, and create profile-related components

---
+++ src/pages/user/sections/profile/BorrowedList.tsx
@@ -0,0 +1,15 @@
+const BorrowedList = () => {
+  return (
+    <section
+      id='borrowed-list'
+      className='flex flex-col gap-3.75 lg:gap-6 max-w-250 w-full m-auto'
+    >
+      <h2 className='font-bold text-display-xs lg:text-display-sm'>
+        Borrowed List
+      </h2>
+      <div></div>
+    </section>
+  );
+};
+
+export default BorrowedList;
